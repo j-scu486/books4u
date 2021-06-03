e59c3d6934fb8645c7ac769f66f6a08b8255c730
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-03 16:45:25 +0900

    add: search form for author or title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <h1>books4u</h1>
 
+    <form action="/" method="GET">
+        <label for="search">Search</label>
+        <input type="text" name="search" id="search">
+        <label for="search_type">Search by</label>
+        <select name="search_type" id="search_type">
+            @foreach ($search_params as $value => $param)
+                <option value="{{ $value }}">{{ $param }}</option>
+            @endforeach
+        </select>
+        <input type="submit" value="submit">
+    </form>
+
     @if (count($books) > 0)
         @foreach ($books as $book)
         <p>{{ $book->title }} by <a href="/author/{{ $book->author->id }}">{{ $book->author->full_name }}</a></p>
