d9ebc6b83b004062181f9f29a75cc3e33b0adcaf
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-03 16:28:40 +0900

    add: base layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <title>Books4u</title>
+    </head>
+    <body>
+        @include('inc.messages')
+        @yield('content')
+    </body>
+</html>
