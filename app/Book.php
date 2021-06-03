a21fc66e6e1ad745a15eec4d8227f75efa4aa8d7
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-03 16:51:22 +0900

    update: types on book model methods

--- app/Book.php
+++ app/Book.php
@@ -11,19 +11,19 @@ class Book extends Model
         return $this->belongsTo('App\Author');
     }
 
-    public static function scopeWithAuthorTable($query)
+    public static function scopeWithAuthorTable(object $query)
     {
         return $query->join('authors', 'authors.id', '=', 'books.author_id');
     }
 
-    public static function searchQuery($url_query)
+    public static function searchQuery(array $url_query)
     {
         return $url_query['search_type'] == 'author'
             ? Book::withAuthorTable()->where('full_name', $url_query['search'])
             : Book::where($url_query['search_type'], $url_query['search']);
     }
 
-    public static function orderQuery($url_query)
+    public static function orderQuery(array $url_query)
     {
         return $url_query['orderby'] == 'author'
             ? Book::withAuthorTable()->orderBy('full_name', $url_query['direction'])
