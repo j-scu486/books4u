7c414f9217095c25ff5ccffa34bf3fd670967e0d
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-04 14:50:39 +0900

    add: aws config

--- resources/views/common/inc/footer.blade.php
+++ resources/views/common/inc/footer.blade.php
@@ -6,7 +6,7 @@
     </div>
     <ul class="footer__links">
         <li class="footer__link"><a href="/">Home</a></li>
-        <li class="footer__link"><a href="#">About</a></li>
+        <li class="footer__link"><a target="_blank" rel="noopener noreferrer" href="https://github.com/j-scu486/books4u">About</a></li>
         <li class="footer__link"><a href="{{ route ('files.index') }}">Downloads</a></li>
     </ul>
 </footer>
--- resources/views/common/inc/nav.blade.php
+++ resources/views/common/inc/nav.blade.php
@@ -6,7 +6,7 @@
     </div>
     <ul class="navbar__links">
         <li class="navbar__link"><a href="/">Home</a></li>
-        <li class="navbar__link"><a href="#">About</a></li>
+        <li class="navbar__link"><a target="_blank" rel="noopener noreferrer" href="https://github.com/j-scu486/books4u">About</a></li>
         <li class="navbar__link"><a href="{{ route ('files.index') }}">Downloads</a></li>
     </ul>
 </nav>
