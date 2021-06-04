aec86bd81c0e31939627110732e6a2ce365914a9
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-04 13:04:39 +0900

    add: author edit view stylings

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,10 +1,17 @@
 @extends('common.layouts.main')
 
 @section('content')
-    <form action="/author/{{ $author->id }}" method="POST">
+    <form class="form form--author" action="/author/{{ $author->id }}" method="POST">
+        <h3 class="header header--title">
+            If {{ $author->full_name }} didn't write all these books... <br>then who did?
+        </h3>
         @csrf
-        <label for="full_name">Author Name</label>
-        <input type="text" name="full_name" id="full_name" value="{{ $author->full_name }}">
-        <input type="submit" value="change">
+        <div class="form-group">
+            <label class="form-label" for="full_name">Author Name</label>
+            <input class="form-control" type="text" name="full_name" id="full_name" value="{{ $author->full_name }}">
+        </div>
+        <div class="form-group">
+            <input class="btn btn--form-submit" type="submit" value="change">
+        </div>
     </form>
 @endsection
--- resources/views/common/inc/messages.blade.php
+++ resources/views/common/inc/messages.blade.php
@@ -1,7 +1,7 @@
 @if(count($errors) > 0)
     @foreach ($errors->all() as $error)
         <div class="message message--error">
-            Error: {{ $error }}
+            {{ $error }}
         </div>
     @endforeach
 @endif
