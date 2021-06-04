6b09da7e4dec4d8339a8f2f41f0434bf6d8547fc
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-04 14:31:28 +0900

    add: author page stylings

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,17 +1,19 @@
 @extends('common.layouts.main')
 
 @section('content')
-    <form class="form form--author" action="/author/{{ $author->id }}" method="POST">
-        <h3 class="header header--title">
-            If {{ $author->full_name }} didn't write all these books... <br>then who did?
-        </h3>
-        @csrf
-        <div class="form-group">
-            <label class="form-label" for="full_name">Author Name</label>
-            <input class="form-control" type="text" name="full_name" id="full_name" value="{{ $author->full_name }}">
-        </div>
-        <div class="form-group">
-            <input class="btn btn--form-submit" type="submit" value="change">
-        </div>
-    </form>
+    <div class="author-container">
+        <form class="form form--author" action="/author/{{ $author->id }}" method="POST">
+            <h3 class="header header--title">
+                If {{ $author->full_name }} didn't write all these books... <br>then who did?
+            </h3>
+            @csrf
+            <div class="form-group">
+                <label class="form-label" for="full_name">Author Name</label>
+                <input class="form-control" type="text" name="full_name" id="full_name" value="{{ $author->full_name }}">
+            </div>
+            <div class="form-group">
+                <input class="btn btn--form-submit" type="submit" value="change">
+            </div>
+        </form>
+    </div>
 @endsection
