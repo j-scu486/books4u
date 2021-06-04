bab3eb27203dba44d8bc065115096e65a9f37010
スクテラ・ジョセフ <s-joseph@vectorinc.co.jp>
2021-06-04 12:05:52 +0900

    add: footer styles

--- resources/views/common/inc/footer.blade.php
+++ resources/views/common/inc/footer.blade.php
@@ -1,3 +1,10 @@
-<footer>
-    This is a footer
+<footer class="footer">
+    <div class="footer__logo">
+        <h4 class="main-header main-header--footer">Books4u <i class="fas fa-book-open"></i></h4>
+    </div>
+    <ul class="footer__links">
+        <li class="footer__link"><a href="#">Home</a></li>
+        <li class="footer__link"><a href="#">About</a></li>
+        <li class="footer__link"><a href="#">Downloads</a></li>
+    </ul>
 </footer>
