378b2737d631cfcecf7cc7dfad28989733f5b4fd
j-scu486 <nocturne486@gmail.com>
2021-06-05 12:56:57 +0900

    update: downloadtest

--- tests/Unit/DownloadTest.php
+++ tests/Unit/DownloadTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Book;
+
 class ValidDownloadTest extends TestCase
 {
     use RefreshDatabase;
@@ -12,7 +14,29 @@ class ValidDownloadTest extends TestCase
     /** @test */
     public function it_downloads_csv()
     {
+        factory(Book::class)->create();
         $response = $this->get('/download/csv/all_data');
+        $response->assertStatus(200);
+        $header = $response->headers->get('content-disposition');
+        $this->assertEquals($header, "attachment; filename=books.csv");
+
+    }
+
+    /** @test */
+    public function it_downloads_csv_title_only()
+    {
+        factory(Book::class)->create();
+        $response = $this->get('/download/csv/title_only');
+        $response->assertStatus(200);
+        $header = $response->headers->get('content-disposition');
+        $this->assertEquals($header, "attachment; filename=books.csv");
+
+    }
+    /** @test */
+    public function it_downloads_csv_author_only()
+    {
+        factory(Book::class)->create();
+        $response = $this->get('/download/csv/author_only');
         $response->assertStatus(200);
         $header = $response->headers->get('content-disposition');
         $this->assertEquals($header, "attachment; filename=books.csv");
@@ -22,6 +46,10 @@ class ValidDownloadTest extends TestCase
     /** @test */
     public function it_downloads_xml()
     {
-        $this->assertTrue(true);
+        factory(Book::class)->create();
+        $response = $this->get('/download/xml/all_data');
+        $response->assertStatus(200);
+        $header = $response->headers->get('content-disposition');
+        $this->assertEquals($header, "attachment; filename=books.xml");
     }
 }
