edffe4f2567cabe51c463a34ee53b183d345b32f
j-scu486 <nocturne486@gmail.com>
2021-06-06 19:30:27 +0900

    fix: files button a link

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -6,30 +6,30 @@
             <div class="file-item">
                 <h3 class="header header--title">Download as CSV</h3>
                 <small class="text-small">Author and Title</small>
-                <button class="btn btn--download"><a href="{{ route ('files.download-csv', 'all_data')}}">Download</a></button>
+                <a href="{{ route ('files.download-csv', 'all_data')}}"><button class="btn btn--download">Download</button></a>
             </div>
             <div class="file-item">
                 <small class="text-small">Title Only</small>
-                <button class="btn btn--download"><a href="{{ route ('files.download-csv', 'title_only')}}">Download</a></button>
+                <a href="{{ route ('files.download-csv', 'title_only')}}"><button class="btn btn--download">Download</button></a>
             </div>
             <div class="file-item">
                 <small class="text-small">Author Only</small>
-                <button class="btn btn--download"><a href="{{ route ('files.download-csv', 'author_only')}}">Download</a></button>
+                <a href="{{ route ('files.download-csv', 'author_only')}}"><button class="btn btn--download">Download</button></a>
             </div>
         </div>
         <div class="file-item-container">
             <div class="file-item">
                 <h3 class="header header--title">Download as XML</h3>
                 <small class="text-small">Author and Title</small>
-                <button class="btn btn--download"><a href="{{ route ('files.download-xml', 'all_data')}}">Download</a></button>
+                <a href="{{ route ('files.download-xml', 'all_data')}}"><button class="btn btn--download">Download</button></a>
             </div>
             <div class="file-item">
                 <small class="text-small">Title Only</small>
-                <button class="btn btn--download"><a href="{{ route ('files.download-xml', 'title_only')}}">Download</a></button>
+                <a href="{{ route ('files.download-xml', 'title_only')}}"><button class="btn btn--download">Download</button></a>
             </div>
             <div class="file-item">
                 <small class="text-small">Author Only</small>
-                <button class="btn btn--download"><a href="{{ route ('files.download-xml', 'author_only')}}">Download</a></button>
+                <a href="{{ route ('files.download-xml', 'author_only')}}"><button class="btn btn--download">Download</button></a>
             </div>
         </div>
     </div>
